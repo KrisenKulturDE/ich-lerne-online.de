000257496cb2e055a6c3a9d39a0edc8607dde682
Sebastian Schendel <sebi@sebi.dev>
2020-03-22 19:25:27 +0100

    SEO Improvement: default description & imageg

---
+++ site/ready.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ProcessWire;
+
+// Add the brand name after the title.
+$wire->addHookAfter('SeoMaestro::renderSeoDataValue', function (HookEvent $event) {
+    $group = $event->arguments(0);
+    $name = $event->arguments(1);
+    $value = $event->arguments(2);
+
+    // Insert default values if empty:
+    if (empty($value)) {
+        if ($name === 'image') {
+            $value = '/site/templates/assets/static/ich-lerne-online-logo.jpg';
+            $event->return = $value;
+        } elseif ($group === 'meta' && $name === 'description') {
+            $value = 'Sammlung von Links, Tools, Tutorials, Tipps und Tricks, um ins Online-Lehren und Lernen einzusteigen und einen Überblick über E-Learning Angebote zu bekommen.';
+            $event->return = $value;
+        }
+    }
+
+    if ($group === 'meta' && $name === 'title') {
+        $event->return = htmlspecialchars(strip_tags($value));
+    } elseif ($name === 'description') {
+        $event->return = htmlspecialchars(trim(str_replace('&nbsp;', ' ', strip_tags($value))));
+    }
+});
